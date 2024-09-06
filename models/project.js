372fd5551e6cf882cb499feb6629b81281d263e3
aaron.fernandez <aaron.fernandez@alisys.net>
2024-09-06 08:57:03 +0200

    Ignorar la carpeta .idea en git

---
+++ models/project.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String,
+    year: Number,
+    langs: [String]
+});
+
+// mongoose coge en name lo pone en minusculas y plural "projects" que coincide con el de mongo
+module.exports = mongoose.model('Project', ProjectSchema);
+// projects --> guarda los documents en la colección
